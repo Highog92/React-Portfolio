1cea617fb681af14d46ad513adacbbaeb3ca1bb2
Highog92 <Madsserritslev@gmail.com>
2023-10-05 12:01:52 +0200

    yeah

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,12 @@
 import './Global.css'
 import { Welcome } from './Components/Welcome/Welcome'
+import GradientButton from './Components/GlowingButton/Special'
 function App() {
 
   return (
     <>
       <Welcome />
-
+      <GradientButton />
     </>
   )
 }
